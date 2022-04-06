a1d80cd0588f8cbedb9adf4555ac331124f0205a
Agustin Bruno <aguzbruno@hotmail.com>
2022-04-06 14:07:53 -0300

    Cors

--- api/src/db.js
+++ api/src/db.js
@@ -3,40 +3,50 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME
+  DATABASE_URL
 } = process.env;
-let sequelize = 
-process.env.NODE_ENV ==='production' 
-? new Sequelize ({
-  database:DB_NAME,
-  dilaect:"postgres",
-  host: DB_HOST,
-  port:5432,
-  username : DB_USER,
-  password : DB_PASSWORD,
-  pool:{
-    max: 3,
-    min: 1,
-    idle: 10000,
-  }, 
-  dialectOptions: {
+
+
+const sequelize = new Sequelize(DATABASE_URL, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectOptions:{
     ssl:{
       require:true,
-      rejectUnathorized:false,
-    }, 
-    keepAlive:true,
-  },
-  ssl:true,
-})
-: new Sequelize (
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/development`,
-  { logging:false, native:false}
-);
+      rejectUnauthorized:false
+    }
+  }
+});
 
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
-//   logging: false, // set to console.log to see the raw SQL queries
-//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-// });
+
+// let sequelize = 
+// process.env.NODE_ENV ==='production' 
+// ? new Sequelize ({
+//   database:DB_NAME,
+//   dilaect:"postgres",
+//   host: DB_HOST,
+//   port:5432,
+//   username : DB_USER,
+//   password : DB_PASSWORD,
+//   pool:{
+//     max: 3,
+//     min: 1,
+//     idle: 10000,
+//   }, 
+//   dialectOptions: {
+//     ssl:{
+//       require:true,
+//       rejectUnathorized:false,
+//     }, 
+//     keepAlive:true,
+//   },
+//   ssl:true,
+// })
+// : new Sequelize (
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/development`,
+//   { logging:false, native:false}
+// );
+
 
 const basename = path.basename(__filename);
 
